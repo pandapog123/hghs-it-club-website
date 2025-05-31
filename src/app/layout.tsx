90e9794cf05b7828c42ba4cb2af4945950e94a79
Daniel Gorra <danielgorra7@gmail.com>
2025-05-31 00:39:40 -0400

    Website Icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,14 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { PropsWithChildren } from "react";
 import TopBar from "./TopBar";
+import Icon from "./icon.png";
 
 export const metadata: Metadata = {
   title: "HGHS I.T. Club",
   description: "Official HGHS I.T. Club Website.",
+  icons: {
+    icon: Icon.src,
+  },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
